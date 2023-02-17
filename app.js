e503b0acd00fcef2aa404428777bead69f98e385
Abi <abi@Mactopia.local>
2023-02-17 14:00:30 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const marvel = {
+  render: () => {
+    const urlAPI = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=deb5f9b06ca2849e8257b8854863ee46&hash=803d33c9afac242112e7b592fb38b8a1';
+    const container = document.querySelector('#marvel-row');
+    let contentHTML = '';
+
+    fetch(urlAPI)
+      .then(res => res.json())
+      .then((json) => {
+        for (const hero of json.data.results) {
+          let urlHero = hero.urls[0].url;
+          contentHTML += `
+            <div class="col-md-4">
+                <a href="${urlHero}" target="_blank">
+                  <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+                </a>
+                <h3 class="title">${hero.name}</h3>
+            </div>`;
+        }
+        container.innerHTML = contentHTML;
+      })
+  }
+};
+marvel.render();
